682e677c71100be6aee66bea774502ccd902c0ea
Michael Araujo <michael.e.araujo@gmail.com>
2026-02-21 00:28:11 -0500

    Add integration testing for request cancellation and fix race condition causing requests not to be cancelled.

--- lsp/nls/tests/main.rs
+++ lsp/nls/tests/main.rs
@@ -1,4 +1,5 @@
-use lsp_types::Url;
+use lsp_server::ErrorCode;
+use lsp_types::{ExecuteCommandParams, Url, request::ExecuteCommand};
 use nickel_lang_utils::project_root::project_root;
 use pretty_assertions::assert_eq;
 use serde_json::json;
@@ -139,31 +140,14 @@ fn refresh_missing_imports() {
 fn cancel_request() {
     let _ = env_logger::try_init();
     let mut harness = TestHarness::new();
-
-    let test_uri = file_url_from_path("/test.ncl").unwrap();
-    harness.send_file(test_uri.clone(), "import \"dep.ncl\"");
-    let diags = harness.wait_for_diagnostics().diagnostics;
-    assert_eq!(2, diags.len());
-    assert!(diags[0].message.contains("import of dep.ncl failed"));
-
-    // Now provide the import.
-    let dep_uri = file_url_from_path("/dep.ncl").unwrap();
-    harness.send_file(dep_uri.clone(), "42");
-
-    // Diagnostics can arrive in any order so we need to figure out
-    // which is which.
-    let (test_diags, dep_diags) = match (
-        harness.wait_for_diagnostics(),
-        harness.wait_for_diagnostics(),
-    ) {
-        (test, dep) if test.uri == test_uri => (test, dep),
-        (dep, test) => (test, dep),
-    };
-
-    assert!(test_diags.diagnostics.is_empty());
-    assert_eq!(test_diags.uri, test_uri);
-    assert!(dep_diags.diagnostics.is_empty());
-    assert_eq!(dep_diags.uri, dep_uri);
+    harness.pause_language_server();
+    let response = harness.request_and_cancel::<ExecuteCommand>(ExecuteCommandParams {
+        command: "eval".into(),
+        arguments: vec![json!({ "uri": "file:///test.ncl"})],
+        ..Default::default()
+    });
+    let err = response.error.unwrap();
+    assert_eq!(err.code, ErrorCode::RequestCanceled as i32);
 }
 
 #[test]
